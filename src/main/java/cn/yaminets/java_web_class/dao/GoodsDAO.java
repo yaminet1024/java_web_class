6199eb202a27207057d318c322c7c99f23a2c07a
yaminet1024 <yaminet@sina.com>
2019-06-19 19:32:01 +0800

    comment update

--- src/main/java/cn/yaminets/java_web_class/dao/GoodsDAO.java
+++ src/main/java/cn/yaminets/java_web_class/dao/GoodsDAO.java
@@ -1,9 +1,13 @@
 package cn.yaminets.java_web_class.dao;
 
 import cn.yaminets.java_web_class.dto.Goods;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Mapper
+@Component
 public interface GoodsDAO {
     int insertGoods(Goods goods);
     List<Goods> getGoodsList(int index,int limit);
--- src/main/java/cn/yaminets/java_web_class/dao/OrdersDAO.java
+++ src/main/java/cn/yaminets/java_web_class/dao/OrdersDAO.java
@@ -3,6 +3,7 @@ package cn.yaminets.java_web_class.dao;
 import cn.yaminets.java_web_class.dto.OrderVo;
 import cn.yaminets.java_web_class.dto.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * @data 2019-06-17 17:31
  **/
 @Mapper
+@Component
 public interface OrdersDAO {
     /**1。 添加新订单*/
     public Integer insertNewOrder(Orders orders);
